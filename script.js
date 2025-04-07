471b7ca67b3f459006c43e512f6a02b6eb96b75a
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-04-07 18:20:09 -0400

    Change initial load count to be less for faster initial load for user

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 10;
 const apiKey = "b1wB1Nk5MmV4BjBveVmnU1J0KWdW5gmghQfanm7oCD4";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -17,6 +17,7 @@ function imageLoaded() {
     if (imagesLoaded === totalImages) {
         ready = true;
         loader.hidden = true;
+        count = 30;
     }
 }
 
